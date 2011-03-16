c26e631333d62785336d4e2bea44c88dac50522a
Newky <richdel1991@gmail.com>
2011-03-16 13:16:28 +0000

    AI DONE

--- Prolog/AI_2.pl
+++ Prolog/AI_2.pl
@@ -1,28 +1,37 @@
-#hdMin(C, S, [], [C|S]).
-#hdMin(C, S, [H | T] , Result ) :-
-	#( C < H, !, hdMin(C, [H | S], T, Result) , 
+arc(N, M, Seed, PCost, Target, Cost) :- M is N * Seed, h(N, Hval, Target),  Cost is 1 + PCost + Hval.
+arc(N, M, Seed, PCost, Target, Cost) :- M is N * Seed + 1, h(N, Hval, Target),  Cost is 2 + PCost + Hval.
 
-		#hdMin(H, [C|S], T Result). 
+is_goal(N, Target ) :- 0 is N mod Target.
 
-arc(N, M, Seed, Cost) :- M is N*Seed, Cost = 1.
-arc(N, M, Seed, Cost) :- M is N*Seed + 1, Cost = 2.
-
-is_goal(N, Target) :- 0 is N mod Target.
 
 h(N, Hvalue, Target) :- is_goal(N, Target), !, Hvalue is 0
-			;
-			Hvalue is 1/N.
-
-search([Node | FRest]) :- is_goal(Node).
-
-search([Node | FRest]) :- setof(X,arc(Node, X), FNode), 
-				add_to_frontier(FNode, FRest, FNew), 
-				search(FNew).
-
-arc(1,2).
-arc(1,3).
-arc(2,4).
-arc(2,5).
-arc(3,6).
+                        ;
+                        Hvalue is 1/N.
 
 
+search([[Node, Cost] | FRest],_,Target, [Node, Cost]) :- is_goal(Node, Target).
+search([[Node, Cost] | FRest],Seed, Target, Found ) :- setof([N, C], arc(Node, N, Seed, Cost,Target, C), FNode), 
+                                add-to-frontier(FNode, FRest, FNew), 
+                                search(FNew, Seed, Target, Found).
+
+
+a-star(Start, Seed, Target, Found) :- setof([N, C], arc(Start, N, Seed, 0 ,Target, C), FNode), 
+                                        add-to-frontier(FNode, [], FNew),
+                                        search(FNew,Seed, Target, Found).
+
+
+
+add-to-frontier([], F, F).
+add-to-frontier([H | T], FRest, FNew) :- insert_sort([H | FRest], FSorted), 
+                                                add-to-frontier(T, FSorted, FNew).
+
+
+
+% This is an insert method.
+insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+i_sort([],Acc,Acc).
+i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
+   
+insert([N,X],[[NY,Y]|T],[[NY,Y]|NT]):-X>Y,insert([N,X],T,NT).
+insert([N,X],[[NY, Y]|T],[[N,X],[NY, Y]|T]):-X=<Y.
+insert(X,[],[X]).
