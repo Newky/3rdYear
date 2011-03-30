f58752400c1987b8510c9882dc778f0789e1f624
Newky <richdel1991@gmail.com>
2011-03-30 15:43:40 +0100

    Original code for bank balance.

---
+++ ComputerArch/tut6/balance.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<time.h>
+
+int balancea = 100;
+int balanceb = 100;
+int balancec = 100;
+
+struct thread_args {
+	int * srcbalance;
+	int * destbalance;
+};
+
+void * bank (void * ptr) {
+	struct thread_args *args = (struct thread_args *)ptr;
+	int * srcbalance = args->srcbalance;
+	int * destbalance = args->destbalance;
+	srand(time(NULL));
+	int i = 0;
+	int amount;
+	for (i = 0; i < 1000000; i++) {
+		amount = rand() % 21 + 1;
+
+		if(*srcbalance > 0){
+			if(*srcbalance < amount)
+				amount = *srcbalance;
+			if (amount > 0) {
+				*srcbalance -= amount;
+				*destbalance += amount;
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int bala, balb, balc;
+	pthread_t t1, t2, t3;
+	struct thread_args t1args, t2args, t3args;
+		
+	t1args.srcbalance = &balancea;
+	t1args.destbalance = &balanceb;
+	t2args.srcbalance = &balanceb;
+	t2args.destbalance = &balancec;
+	t3args.srcbalance = &balancec;
+	t3args.destbalance = &balancea;
+
+	bala = pthread_create(&t1, NULL, bank, (void *) &t1args);
+	balb = pthread_create(&t2, NULL, bank, (void *) &t2args);
+	balc = pthread_create(&t3, NULL, bank, (void *) &t3args);
+
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
+
+	printf("%d %d %d \n", balancea, balanceb, balancec);
+
+	return 0;
+}
